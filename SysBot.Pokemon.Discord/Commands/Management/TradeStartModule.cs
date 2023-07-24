25453607bd5087a8ecd5e1eddcf2c3f3fa5029dd
Dai <itsdaivr@gmail.com>
2023-07-24 02:22:12 -0600

    a

--- SysBot.Pokemon.Discord/Commands/Management/TradeStartModule.cs
+++ SysBot.Pokemon.Discord/Commands/Management/TradeStartModule.cs
@@ -76,7 +76,7 @@ namespace SysBot.Pokemon.Discord
 
             Action<PokeRoutineExecutorBase, PokeTradeDetail<T>> l = Logger;
             SysCord<T>.Runner.Hub.Queues.Forwarders.Add(l);
-            static string GetMessage(PokeRoutineExecutorBase bot, PokeTradeDetail<T> detail) => $"> [{DateTime.Now:hh:mm:ss}] - {bot.Connection.Label} ahora esta tradeando (ID {detail.ID}) {detail.Trainer.TrainerName}";
+            static string GetMessage(PokeRoutineExecutorBase bot, PokeTradeDetail<T> detail) => $"> ✔ [{DateTime.Now:hh:mm:ss}] ➜ **{bot.Connection.Label}** ahora esta tradeando con **{detail.Trainer.TrainerName}**. (ID **{detail.ID}**)";
 
             var entry = new TradeStartAction(cid, l, c.Name);
             Channels.Add(cid, entry);
